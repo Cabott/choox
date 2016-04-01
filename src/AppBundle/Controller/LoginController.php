72ca167179bb4bdd04e395b58b21a915a91069e1
Maria Soliman <maria.soliman@eventim.de>
2016-04-02 01:06:50 +0200

    add twitter bootstrap! and tried to use less but didn't work yet

--- src/AppBundle/Controller/LoginController.php
+++ src/AppBundle/Controller/LoginController.php
@@ -9,11 +9,20 @@ class LoginController extends Controller
 {
 
     /**
-     * @Route("/login/{user}")
-     */
+    * @Route("/login/{user}", defaults={"user" = ""}, requirements={"user": "\d+"})
+    */
     public function loginAction($user)
     {
       $html=$this->render('login/index.html.twig',['user'=> $user]);
+        return new Response($html);
+    }
+
+    /**
+     * @Route("/login/{user}", defaults={"user" = ""})
+     */
+    public function helloAction($user)
+    {
+        $html=$this->render('login/hello.html.twig',['user'=> $user]);
         return new Response($html);
     }
 }
