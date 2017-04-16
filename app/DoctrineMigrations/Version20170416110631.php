39155307c55a82a649c6d009c6be186a29c20b06
Maria Soliman <caddy@choox.de>
2017-04-16 13:10:56 +0200

    Make sure migrations can be used correctly.
    This is the start of the app database.
    You start with an empty database.
    After running the migration file, there are two tables, users and logos.

---
+++ app/DoctrineMigrations/Version20170416110631.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170416110631 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE logo (id INT AUTO_INCREMENT NOT NULL, user INT NOT NULL, team_name VARCHAR(100) NOT NULL, team_name_alternatives TEXT DEFAULT NULL COMMENT \'(DC2Type:json_array)\', team_country VARCHAR(100) NOT NULL, team_hint VARCHAR(300) NOT NULL, times_solved INT NOT NULL, times_missed INT NOT NULL, approved TINYINT(1) NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, original_logo VARCHAR(100) NOT NULL, altered_logo VARCHAR(100) NOT NULL, INDEX IDX_E48E9A138D93D649 (user), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE `user` (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(180) NOT NULL, username_canonical VARCHAR(180) NOT NULL, email VARCHAR(180) NOT NULL, email_canonical VARCHAR(180) NOT NULL, enabled TINYINT(1) NOT NULL, salt VARCHAR(255) DEFAULT NULL, password VARCHAR(255) NOT NULL, last_login DATETIME DEFAULT NULL, confirmation_token VARCHAR(180) DEFAULT NULL, password_requested_at DATETIME DEFAULT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\', favourite_team VARCHAR(64) NOT NULL, points INT NOT NULL, UNIQUE INDEX UNIQ_8D93D64992FC23A8 (username_canonical), UNIQUE INDEX UNIQ_8D93D649A0D96FBF (email_canonical), UNIQUE INDEX UNIQ_8D93D649C05FB297 (confirmation_token), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE logo ADD CONSTRAINT FK_E48E9A138D93D649 FOREIGN KEY (user) REFERENCES `user` (id)');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE logo DROP FOREIGN KEY FK_E48E9A138D93D649');
+        $this->addSql('DROP TABLE logo');
+        $this->addSql('DROP TABLE `user`');
+    }
+}
